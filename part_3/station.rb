4501d59b0b49b7cb0f9b0e081367bbe62d6b45af
Dima <sumskoydima@gmail.com>
2017-03-18 01:41:48 +0300

    Fixed passenger/freight methods

--- part_3/station.rb
+++ part_3/station.rb
@@ -12,11 +12,11 @@ class Station
   end
   
   def show_passenger_trains
-    passenger_trains = [] << @trains.select { |train| train.type == "passenger" }
+    @trains.select { |train| train.type == "passenger" }
   end
 
   def show_freight_trains
-    freight_trains = [] << @trains.select { |train| train.type == "freight" }
+    @trains.select { |train| train.type == "freight" }
   end
 
   def send_train(number_of_train)
