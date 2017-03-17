e71e3b13328940f88c17598351823f317a64bfef
Dima <sumskoydima@gmail.com>
2017-03-17 20:28:54 +0300

    Fixed condition

--- part_3/route.rb
+++ part_3/route.rb
@@ -11,7 +11,7 @@ class Route
   end
 
   def remove_station(station)
-    if station != @first_station && station != @last_station
+    if station != @stations[0] && station != @stations[1]
       @stations.delete(station)
     else
       puts "Error"
