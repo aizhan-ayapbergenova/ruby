05dc5470fced8a810828942f8c0b4277348015e2
Dima <sumskoydima@gmail.com>
2017-03-18 04:25:54 +0300

    Fixed forward/backward & *station methods

--- part_3/train.rb
+++ part_3/train.rb
@@ -1,14 +1,13 @@
 class Train
 
   attr_reader :speed, :type, :number, :carriage_quantity
-  attr_writer :route
 
   def initialize(number, type, carriage_quantity)
     @number = number
     @type   = type
     @carriage_quantity = carriage_quantity
     @speed = 0
-    @station_index = 0
+    @station = nil
     @route = nil
   end
 
@@ -18,6 +17,11 @@ class Train
 
   def reset_speed
     @speed = 0
+  end
+
+  def route=(route)
+    @route = route
+    @station = @route.stations.first
   end
 
   def attach_vagon
@@ -37,38 +41,32 @@ class Train
   end
 
   def forward
-    if @station_index < @route.stations.size - 1
-      @station_index += 1
-    else
-      puts "Error"
+    if next_station
+      current_station.send_train(self)
+      next_station.take_the_train(self)
     end
   end
 
   def backward
-    if @station_index > 0
-      @station_index -= 1
-    else
-      puts "Error"
+    if previous_station
+      current_station.send_train(self)
+      previous_station.take_the_train(self)
     end
   end
 
   def previous_station
-    if @station_index > 0
-      @route.stations[@station_index - 1]
-    else
-      puts "Error"
+    previous_index = @route.stations.index(@station) - 1
+    if previous_index > 0
+      @route.stations[previous_index]
     end
   end
 
   def current_station
-    @route.stations[@station_index]
+    @station
   end
 
   def next_station
-    if @station_index < @route.stations.size - 1
-      @route.stations[@station_index + 1]
-    else
-      puts "Error"
-    end
+    next_index = @route.stations.index(@station) + 1
+    @route.stations[next_index]
   end
 end
