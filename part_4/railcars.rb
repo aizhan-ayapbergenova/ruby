c1188b5cbd7cba93e46b39ee9d7b79b07578bc19
Dima <sumskoydima@gmail.com>
2017-03-21 22:14:34 +0300

    Refactored

--- part_4/railcars.rb
+++ part_4/railcars.rb
@@ -1,25 +1,19 @@
 class Railcar
-
   attr_reader :type
 
   def initialize
     self.type
   end
-
 end
 
 class PassengerRailcar < Railcar
-
   def type
     @type = "passenger"
   end
-
 end
 
 class CargoRailcar < Railcar
-
   def type
     @type = "cargo"
   end
-
 end
