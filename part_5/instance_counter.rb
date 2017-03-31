2b7c2580fe08e3b2b891e9b429590e0ceb251da5
Dima <sumskoydima@gmail.com>
2017-03-31 16:49:08 +0300

    Fixed class-method

--- part_5/instance_counter.rb
+++ part_5/instance_counter.rb
@@ -5,13 +5,12 @@ module InstanceCounter
   end
 
   module ClassMethods
-    @@instances = 0
     def instances
-      @@instances
+      @instances
     end
 
     def instances=(instance)
-      @@instances = instance
+      @instances = instance
     end
   end
 
@@ -20,6 +19,7 @@ module InstanceCounter
     protected
 
     def register_instance
+      self.class.instances ||= 0
       self.class.instances += 1
     end
   end
