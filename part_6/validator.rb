c8a48241949d455fa3af2ea43dd8eb01bbba649c
Dima <sumskoydima@gmail.com>
2017-04-06 23:02:45 +0300

    Modified methods name

--- part_6/validator.rb
+++ part_6/validator.rb
@@ -3,27 +3,27 @@ module Validator
   ATTR_NAME = /^([a-z0-9]\s?-?)*$/i
   NUMBER_FORMAT = /^[a-z0-9]{3}-?[a-z0-9]{2}$/i
 
-  def name_valid?
-    name_validate!
+  def station_valid?
+    station_validate!
   rescue
     false
   end
 
-  def number_valid?
-    number_validate!
+  def train_valid?
+    train_validate!
   rescue
     false
   end
 
   protected
 
-  def name_validate!
+  def station_validate!
     raise 'Enter the name of the station' if name.length.zero?
     raise 'The name is incorrect' if name !~ ATTR_NAME
     true
   end
 
-  def number_validate!
+  def train_validate!
     raise "The train number must have letters or numbers and look like this: XXX-XX or XXXXX" if number !~ NUMBER_FORMAT
     puts "The train №#{number} successfully created"
   end
