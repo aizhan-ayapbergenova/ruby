c186faa240b804f7ba13781ec617bfdbf0537b14
Dima <sumskoydima@gmail.com>
2017-04-08 19:13:45 +0300

    Added validate methods

--- part_6/route.rb
+++ part_6/route.rb
@@ -1,7 +1,10 @@
 class Route
-  attr_reader :stations
+  attr_reader :stations, :first_station, :last_station
 
   def initialize(first_station, last_station)
+    @first_station = first_station
+    @last_station  = last_station
+    validate!
     @stations = [first_station, last_station]
   end
 
@@ -13,5 +16,16 @@ class Route
     if station != @stations.first && station != @stations.last
       @stations.delete(station)
     end
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  def validate!
+    raise "The route must contain only stations" if first_station.class.to_s != "Station" || last_station.class.to_s != "Station"
   end
 end
