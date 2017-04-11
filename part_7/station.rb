661bb8fddfdb88de3cb74a3f66e5284bbe3f6d7c
Dima <sumskoydima@gmail.com>
2017-04-11 23:25:57 +0300

    Added lesson 7 files with new methods

---
+++ part_7/station.rb
@@ -0,0 +1,48 @@
+class Station
+  attr_reader :trains, :name
+
+  ATTR_NAME = /^([a-z\d]\s?-?)*$/i
+
+  @stations = []
+
+  def self.all
+    @stations
+  end
+
+  def initialize(name)
+    @name = name
+    validate!
+    @trains = []
+    self.class.all << self
+  end
+
+  def take_train(train)
+    @trains << train
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+
+  def station_trains(type)
+    @trains.select { |train| train.class.to_s == type }
+  end
+
+  def pass_train(&block)
+    @trains.each do |train|
+      block.call(train)
+    end
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  def validate!
+    raise 'Enter the name of the station' if name.length.zero?
+    raise 'The name is incorrect' if name !~ ATTR_NAME
+  end
+end
---
+++ part_7/train.rb
@@ -0,0 +1,97 @@
+require_relative 'manufacturer'
+
+class Train
+  @@trains = {}
+
+  include Manufacturer
+
+  attr_reader :speed, :railcars, :number
+
+  NUMBER_FORMAT = /^[a-z\d]{3}-?[a-z\d]{2}$/i
+
+  def self.find(number)
+    @@trains[number]
+  end
+
+  def initialize(number)
+    @number = number
+    validate!
+    @railcars = []
+    @speed = 0
+    @@trains[number] = self
+  end
+
+  def increase_speed
+    @speed += 10
+  end
+
+  def reset_speed
+    @speed = 0
+  end
+
+  def route=(route)
+    @route = route
+    @station_index = 0
+  end
+
+  def forward
+    if next_station
+      current_station.send_train(self)
+      next_station.take_train(self)
+      @station_index += 1
+    end
+  end
+
+  def backward
+    if previous_station
+      current_station.send_train(self)
+      previous_station.take_train(self)
+      @station_index -= 1
+    end
+  end
+
+  def current_station
+    @route.stations[@station_index]
+  end
+
+  def previous_station
+    @route.stations[@station_index - 1] if @station_index > 0
+  end
+
+  def next_station
+    @route.stations[@station_index + 1]
+  end
+
+  def stop?
+    speed.zero?
+  end
+
+  def attach_railcar(railcar)
+    if railcar.class.to_s == @railcar_type && stop?
+      @railcars << railcar
+    else
+      raise "Type mismatch!"
+    end
+  end
+
+  def unhook_railcar
+    @railcars.pop if stop?
+  end
+
+  def pass_railcar(&block)
+    @railcars.each do |railcar|
+      block.call(railcar)
+    end
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  def validate!
+    raise "The train number must have letters or numbers and look like this: XXX-XX or XXXXX" if number !~ NUMBER_FORMAT
+  end
+end
