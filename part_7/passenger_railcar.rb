dbcb68b9c51a1b1595632a21cb1797fb51b62ced
Dima <sumskoydima@gmail.com>
2017-04-13 13:20:39 +0300

    Changed getter name

--- part_7/passenger_railcar.rb
+++ part_7/passenger_railcar.rb
@@ -1,20 +1,21 @@
 require_relative 'railcar'
 
 class PassengerRailcar < RailCar
-  attr_reader :free_seats, :taken_seats
+  attr_reader :free_seats, :reserved_seats
 
   def initialize(seats)
     @free_seats = seats
-    @taken_seats = 0
+    @reserved_seats = 0
     raise "Amount of seats can't be 0 or negative" if seats == 0 || seats < 0
+    super
   end
 
   def take_seat
     if @free_seats > 0
       @free_seats -= 1
-      @taken_seats += 1
+      @reserved_seats += 1
     else
-      raise "No vacant seats!"
+      puts "No vacant seats!"
     end
   end
 end
