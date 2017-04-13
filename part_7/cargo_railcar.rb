b1cdd35cfe8d0129e738729f44faf9e3e0fed358
Dima <sumskoydima@gmail.com>
2017-04-13 13:20:13 +0300

    Changed getter name

--- part_7/cargo_railcar.rb
+++ part_7/cargo_railcar.rb
@@ -1,20 +1,21 @@
 require_relative 'railcar'
 
 class CargoRailcar < RailCar
-  attr_reader :free_volume, :taken_volume
+  attr_reader :free_volume, :filled_volume
 
   def initialize(volume)
     @free_volume = volume.to_f
-    @taken_volume = 0
+    @filled_volume = 0
+    super
   end
 
   def fill(volume)
     raise "Available volume is - #{@free_volume}" if volume > @free_volume
     if @free_volume > 0
       @free_volume -= volume
-      @taken_volume += volume
+      @filled_volume += volume
     else
-      raise "Fully filled"
+      puts "Fully filled"
     end
   end
 end
