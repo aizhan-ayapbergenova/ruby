f123bc39a1e88e7b8adccb339a0272f7063e9b98
Dima <sumskoydima@gmail.com>
2017-04-30 23:54:38 +0300

    Refactored

--- part_9/accessors.rb
+++ part_9/accessors.rb
@@ -2,12 +2,12 @@ module Accessors
   def attr_accessor_with_history(*args)
     args.each do |method_name|
       method_var = "@#{method_name}".to_sym
-      storage_var = []
-      define_method("#{method_name}_history") { storage_var }
+      method_history = []
+      define_method("#{method_name}_history") { method_history }
       define_method(method_name) { instance_variable_get(method_var) }
       define_method("#{method_name}=".to_sym) do |value|
         instance_variable_set(method_var, value)
-        storage_var << value
+        method_history << value
       end
     end
   end
@@ -16,7 +16,7 @@ module Accessors
     var_name = "@#{attr_name}".to_sym
     define_method(attr_name) { instance_variable_get(var_name) }
     define_method("#{attr_name}=".to_sym) do |value|
-      raise if value.class != attr_class
+      raise unless value.class.is_a?(attr_class)
       instance_variable_set(var_name, value)
     end
   end
