62c3cad951468655ce107e33503f2a209fa7cc54
Dima <sumskoydima@gmail.com>
2017-05-03 19:40:19 +0300

    Modified 'history' method

--- part_9/accessors.rb
+++ part_9/accessors.rb
@@ -3,11 +3,13 @@ module Accessors
     args.each do |method_name|
       method_var = "@#{method_name}".to_sym
       method_history = []
-      define_method("#{method_name}_history") { method_history }
       define_method(method_name) { instance_variable_get(method_var) }
       define_method("#{method_name}=".to_sym) do |value|
+        method_history << instance_variable_get(method_var)
         instance_variable_set(method_var, value)
-        method_history << value
+      end
+      define_method("#{method_name}_history".to_sym) do 
+        method_history.select { |name| name != nil }
       end
     end
   end
