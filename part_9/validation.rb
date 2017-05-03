d5e341dd433784497dc45c5dc67c00b80f7435d4
Dima <sumskoydima@gmail.com>
2017-05-03 19:42:25 +0300

    Hash to array & modified validate!

--- part_9/validation.rb
+++ part_9/validation.rb
@@ -5,32 +5,32 @@ module Validation
   end
 
   module ClassMethods
-    def validate(attr_name, validation_type, validation_format = nil)
-      @options ||= {}
-      @options[attr_name] = { "#{validation_type}": validation_format }
+    def validate(attr_name, validation_type, validation_standart = nil)
+      @validations ||= []
+      @validations << [attr_name, validation_type, validation_standart]
     end
   end
 
-  module InstanceMethods 
+  module InstanceMethods
     def validate!
-      self.class.instance_variable_get(:@options).each do |attr_name, attr_validation| 
-        attr_name = instance_variable_get("@#{attr_name}")
-        validation_type = attr_validation.keys[0]
-        validation_standart = attr_validation.values[0]
-        self.send(validation_type, attr_name, validation_standart)
+      self.class.instance_variable_get(:@validations).each do |validation_clause|
+        attr_name = instance_variable_get("@#{validation_clause[0]}")
+        validation_type = validation_clause[1]
+        validation_standart = validation_clause[2]
+        send(validation_type, attr_name, validation_standart)
       end
-    end 
+    end
 
-    def presence(attribute, *args)
-      raise "Wrong name" if attribute.nil? || attribute.empty?
+    def presence(attribute, *_args)
+      raise 'Wrong name' if attribute.nil? || attribute.empty?
     end
 
     def format(attribute, standart)
-      raise "Wrong format" if attribute.to_s !~ standart
+      raise 'Wrong format' if attribute.to_s !~ standart
     end
 
     def type(attribute, attribute_class)
-      raise "Wrong type" if attribute.class != attribute_class
+      raise 'Wrong type' if attribute.class != attribute_class
     end
 
     def valid?
